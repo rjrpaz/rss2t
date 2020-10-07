ec897bbee9fadcc9bd8ef637e8d6edffb1265ebc
Roberto J. R. Paz <rjrpaz@gmail.com>
2020-10-07 20:29:03 -0300

    Added tag to the telegram message

--- rss2t/telegram.py
+++ rss2t/telegram.py
@@ -10,10 +10,10 @@ def cleanhtml(raw_html):
 	cleantext = re.sub(cleanr, '', raw_html)
 	return cleantext
 
-def send_message(link, summary):
+def send_message(tag, link, summary):
 	config = configparser.ConfigParser()
 	config.read(config_ini)
 
-	message = '\n'.join([link, cleanhtml(summary)])
+	message = '\n'.join([tag,link, cleanhtml(summary)])
 #	print (message)
 	requests.get(f'https://api.telegram.org/bot{config["DEFAULT"]["bot_token"]}/sendMessage?chat_id={config["DEFAULT"]["channel_id"]}&text={message}')
